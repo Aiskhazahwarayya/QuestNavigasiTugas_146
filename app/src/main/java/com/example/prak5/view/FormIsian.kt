0966f165b8607ba55fbe03c290a2dad46e125f51
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-07 14:31:32 +0700

    Menambahkan header Formulir Pendaftaran

--- app/src/main/java/com/example/prak5/view/FormIsian.kt
+++ app/src/main/java/com/example/prak5/view/FormIsian.kt
@@ -1,8 +1,25 @@
 package com.example.prak5.view
+import androidx.compose.foundation.background
+import androidx.compose.foundation.content.MediaType.Companion.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.Text
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FormPendaftaran(
@@ -19,4 +36,31 @@ fun FormPendaftaran(
 
     val backgroundLavender = Color(0xFFF3E5F5)
     val purpleButton = Color(0xFF7C3AED)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundLavender)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(110.dp)
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(Color(0xFFD1B3FF), Color(0xFFB388FF))
+                    )
+                ),
+            contentAlignment = Alignment.CenterStart
+        ) {
+            Text(
+                text = "Formulir Pendaftaran",
+                color = Color.White,
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp,
+                modifier = Modifier
+                    .padding(start = 20.dp, top = 16.dp)
+            )
+        }
+    }
 }
