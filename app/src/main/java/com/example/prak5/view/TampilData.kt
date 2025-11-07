e4f949f63aa3d3f3f54272dd93ab0537f8b89bef
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-07 16:07:26 +0700

    membuat Composable TampilData

---
+++ app/src/main/java/com/example/prak5/view/TampilData.kt
@@ -0,0 +1,13 @@
+package com.example.prak5.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TampilData(
+    namaLengkap: String,
+    jenisKelamin: String,
+    statusPerkawinan: String,
+    alamat: String,
+    onBerandaClick: () -> Unit,
+    onFormulirClick: () -> Unit
+) {}
