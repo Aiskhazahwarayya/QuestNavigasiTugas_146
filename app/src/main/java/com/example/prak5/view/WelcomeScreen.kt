e340d85caee90343935a919ce3ecbe09c7184738
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-07 15:57:15 +0700

    menambahkan teks judul "Selamat Datang"

--- app/src/main/java/com/example/prak5/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/prak5/view/WelcomeScreen.kt
@@ -2,11 +2,15 @@ package com.example.cn.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -25,6 +29,17 @@ fun WelcomeScreen(
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(top = 40.dp)
+        ) {
+            Text(
+                text = "Selamat Datang",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Normal,
+                color = colorPurple700,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
