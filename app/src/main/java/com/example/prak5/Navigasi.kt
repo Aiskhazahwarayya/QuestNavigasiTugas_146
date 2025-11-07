4a4b06360cfa3991f47fa21a4f0992a519b50f77
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-07 19:28:32 +0700

    menambahkan navigasi dari WelcomeScreen

--- app/src/main/java/com/example/prak5/Navigasi.kt
+++ app/src/main/java/com/example/prak5/Navigasi.kt
@@ -2,13 +2,18 @@ package com.example.prak5
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cn.view.WelcomeScreen
 
 enum class Halaman {
     Welcome,
     TampilData,
     FormPendaftaran
 }
+
 
 @Composable
 fun DataApp(
@@ -19,5 +24,13 @@ fun DataApp(
         navController = navController,
         startDestination = Halaman.Welcome.name,
         modifier = modifier
-    ) {}
+    ) {
+        composable(route = Halaman.Welcome.name) {
+            WelcomeScreen(
+                onSubmitClick = {
+                    navController.navigate(Halaman.TampilData.name)
+                }
+            )
+        }
+    }
 }
