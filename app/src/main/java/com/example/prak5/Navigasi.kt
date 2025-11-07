bb69d8f3a87fa3a5c93f0a50b7cd81f5b7b04d56
Aiskhazahwarayya <aiskharayya307@gmail.com>
2025-11-07 19:26:30 +0700

    membuat Composable DataApp dengan NavHost dan startDestination

--- app/src/main/java/com/example/prak5/Navigasi.kt
+++ app/src/main/java/com/example/prak5/Navigasi.kt
@@ -1,7 +1,23 @@
 package com.example.prak5
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+
 enum class Halaman {
     Welcome,
     TampilData,
     FormPendaftaran
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Welcome.name,
+        modifier = modifier
+    ) {}
 }
